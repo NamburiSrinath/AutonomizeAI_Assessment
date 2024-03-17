aef0619e013ad0b624ecf0efa5028fc4e34a48df
NamburiSrinath <namburisrinath@gmail.com>
2024-03-17 17:30:13 -0500

    Streamlit code cleaned

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 from functools import partial
 
 # some config
-LSTM_INPUT_SIZE = 128
-LSTM_HIDDEN = 128
+LSTM_INPUT_SIZE = 6
+LSTM_HIDDEN = 256
 LSTM_LAYER = 2
 
 # Model
@@ -15,37 +15,43 @@ class CpGPredictor(torch.nn.Module):
         super(CpGPredictor, self).__init__()
         # TODO complete model, you are free to add whatever layers you need here
         # We do need a lstm and a classifier layer here but you are free to implement them in your way
-        self.lstm = nn.LSTM(LSTM_INPUT_SIZE, LSTM_HIDDEN, LSTM_LAYER, batch_first=True)
+        self.lstm = nn.LSTM(LSTM_INPUT_SIZE, LSTM_HIDDEN, LSTM_LAYER, batch_first=True,
+                            dropout=0.2, bidirectional=True)
 
+        self.dropout = nn.Dropout(0.2)
         # Have a hidden layer to map non-linearities!
-        self.hidden = nn.Linear(LSTM_HIDDEN, LSTM_HIDDEN)
-
+        # NOTE: If it's BiLSTM -> Need to have *2 !! 
+        self.hidden1 = nn.Linear(LSTM_HIDDEN*2, LSTM_HIDDEN//2)
+        self.relu = nn.ReLU()
+        self.hidden2 = nn.Linear(LSTM_HIDDEN//2, LSTM_HIDDEN//4)
         # Final classification layer
-        self.classifier = nn.Linear(LSTM_HIDDEN, 1)
+        self.classifier = nn.Linear(LSTM_HIDDEN//4, 1)
 
     def forward(self, x):
         # TODO complete forward function
         lstm_out, _ = self.lstm(x)
-        # print(lstm_out.shape) # batch_size x LSTM_HIDDEN
-        # This might not make sense as our tensor is 2D for eg: (32, 128)
-        # TODO: Can we make it to 3D tensor and model! Think more on pros and cons! 
-        # last_output = lstm_out[:, -1, :]
-        output = lstm_out
+        last_output = lstm_out[:, -1, :]
+        output = last_output
 
-        output = self.hidden(output)
+        output = self.hidden1(output)
+        output = self.relu(output)
+        output = self.dropout(output)
+
+        output = self.hidden2(output)
+        output = self.relu(output)
+        output = self.dropout(output)
+        
         logits = self.classifier(output)
-        # print(logits.shape) # batch_size x 1
-        # print(logits)
         return logits.squeeze(1) # removing 1
 
 # Load the trained model
 model = CpGPredictor()
-model.load_state_dict(torch.load('trial_model_part1.pkl'))
+model.load_state_dict(torch.load('best_model_part2.pkl', map_location='cpu'))
 model.eval()
 
 alphabet = 'NACGT'
-dna2int = { a: i for a, i in zip(alphabet, range(6))}
-int2dna = { i: a for a, i in zip(alphabet, range(6))}
+dna2int = { a: i for a, i in zip(alphabet, range(1, 6))}
+int2dna = { i: a for a, i in zip(alphabet, range(1, 6))}
 dna2int.update({"pad": 0})
 int2dna.update({0: "<pad>"})
 
@@ -55,17 +61,16 @@ dnaseq_to_intseq = partial(map, dna2int.get)
 # Define function to predict CpG sites
 def predict_cpg(sequence):
     max_len = 128
-    # print(sequence)
     encoded_seq = list(dnaseq_to_intseq(sequence))
     encoded_seq = encoded_seq[:max_len] + [0] * (max_len - len(encoded_seq))
-    encoded_seq_t = torch.tensor(encoded_seq, dtype=torch.float32).unsqueeze(0)
-    # print(encoded_seq_t)
-    # print(encoded_seq_t.shape)
+    encoded_seq_t = torch.nn.functional.one_hot(torch.tensor(encoded_seq), num_classes=len(alphabet)+1).float()
+    encoded_seq_t = encoded_seq_t.unsqueeze(0)
+    
+    print(encoded_seq_t.shape)
     with torch.no_grad():
         pred = model(encoded_seq_t)  
     # Not rounding it to nearest integer!
     return pred.item()
-    # return int(pred.item())
 
 # Streamlit app
 st.title('CpG Site Predictor')
